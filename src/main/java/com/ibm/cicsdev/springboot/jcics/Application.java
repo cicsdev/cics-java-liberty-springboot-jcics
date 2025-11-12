fffbf8f0eab279e686059a9da547fef59132e285
manvisingh55 <manvisingh55@9.79.157.122>
2025-11-12 13:26:56 +0530

    Added code

--- src/main/java/com/ibm/cicsdev/springboot/jcics/Application.java
+++ src/main/java/com/ibm/cicsdev/springboot/jcics/Application.java
@@ -1,23 +1,13 @@
-/* Licensed Materials - Property of IBM                                   */
-/*                                                                        */
-/* SAMPLE                                                                 */
-/*                                                                        */
-/* (c) Copyright IBM Corp. 2020,2022 All Rights Reserved                       */
-/*                                                                        */
-/* US Government Users Restricted Rights - Use, duplication or disclosure */
-/* restricted by GSA ADP Schedule Contract with IBM Corp                  */
-/*                                                                        */
-
 package com.ibm.cicsdev.springboot.jcics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class Application 
-{
-	public static void main(String[] args) 
-	{
-		SpringApplication.run(Application.class, args);
-	}
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
