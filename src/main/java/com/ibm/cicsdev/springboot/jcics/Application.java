fc96135d6e3f565481d8f85646d7cd3975992acc
manvisingh55 <manvisingh55@mac.in.ibm.com>
2026-01-28 02:13:00 +0530

    this is the edited commit after comments on previous one

--- src/main/java/com/ibm/cicsdev/springboot/jcics/Application.java
+++ src/main/java/com/ibm/cicsdev/springboot/jcics/Application.java
@@ -1,3 +1,13 @@
+/* Licensed Materials - Property of IBM                                   */
+/*                                                                        */
+/* SAMPLE                                                                 */
+/*                                                                        */
+/* (c) Copyright IBM Corp. 2020,2022 All Rights Reserved                       */
+/*                                                                        */
+/* US Government Users Restricted Rights - Use, duplication or disclosure */
+/* restricted by GSA ADP Schedule Contract with IBM Corp                  */
+/*                                                                        */
+
 package com.ibm.cicsdev.springboot.jcics;
 
 import org.springframework.boot.SpringApplication;
@@ -5,8 +15,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
+public class Application 
+{
+	
+    public static void main(String[] args) 
+    {
         SpringApplication.run(Application.class, args);
     }
 }
